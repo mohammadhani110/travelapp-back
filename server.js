bbffea230d175b6a9158332818ca992a40ea2380
mohammadhani110 <mohammadhani110@gmail.com>
2023-01-07 00:15:21 +0500

    port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 connectDB();
 
