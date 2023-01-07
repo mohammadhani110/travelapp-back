eb9080b9488401a8d19e7ecd12bdd20bf01bd5b1
mohammadhani110 <mohammadhani110@gmail.com>
2023-01-07 17:20:06 +0500

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,91 +1,92 @@
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
-const asyncHandler = require("express-async-handler")
-const User = require("../models/userModel")
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 //@desc Register new user
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-        res.status(400)
-        throw new Error("Please fill all the fields")
-    }
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
 
-    // Check User Exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-        res.status(400)
-        throw new Error("User already exists");
-    }
+  // Check User Exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    // Hash Password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create User
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword
-    })
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
-    if (user) {
-        res.status(201).json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id)
-        })
-    } else {
-        res.status(400)
-        throw new Error("Invalid User data")
-    }
-
-})
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+});
 
 //@desc Authenticate a user
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-        res.status(201).json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id)
-        })
-    } else {
-        res.status(400)
-        throw new Error("Invalid credentials!")
-    }
-})
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else if (!user) {
+    res.status(400);
+    throw new Error("User not found!");
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials!");
+  }
+});
 
 //@desc Get user data
 //@route GET /api/users/me
 //@access Public
 const getMe = asyncHandler(async (req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id)
-    res.status(200)
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200);
 
-    res.json({ _id, name, email })
-})
+  res.json({ _id, name, email });
+});
 
 // Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "30d"
-    })
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
-    registerUser,
-    loginUser,
-    getMe
-}
+  registerUser,
+  loginUser,
+  getMe,
+};
