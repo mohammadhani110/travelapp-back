3885117e1ad25d19508007f73f7a8364b56998cd
mohammadhani110 <mohammadhani110@gmail.com>
2023-01-07 06:56:34 +0500

    stripe integration

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const colors = require("colors");
-const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -12,30 +12,70 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
-app.options("*", cors());
+// app.use(cors());
+// app.options("*", cors());
 app.use(
   cors({
     credentials: true,
     origin: [
       "http://localhost:3000",
-      "https://travelapp-back.up.railway.app",
-      "https://travelapp-back.cyclic.app",
+      "https://travelapp-front.vercel.app",
+      //   "https://travelapp-back.up.railway.app",
+      //   "https://travelapp-back.cyclic.app",
     ],
-    default: "http://localhost:3000",
   })
 );
-// app.all("*", function (req, res, next) {
-//   const origin = cors.origin.includes(req.header("origin").toLowerCase())
-//     ? req.headers.origin
-//     : cors.default;
-//   res.header("Access-Control-Allow-Origin", origin);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const storeItems = new Map([
+//   [
+//     "6399d43f0cef9cc11b08c65e",
+//     { priceInCents: 10000, name: "Learn React Today" },
+//   ],
+//   [
+//     "6395c2eabd48c13ad434b892",
+//     { priceInCents: 20000, name: "Learn CSS Today" },
+//   ],
+// ]);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: req.body.name,
+              images: [req.body.image],
+            },
+            unit_amount: req.body.price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/tours`,
+      //   line_items: req.body.items.map((item) => {
+      //     const storeItem = storeItems.get(item.id);
+      //     return {
+      //       price_data: {
+      //         currency: "usd",
+      //         product_data: {
+      //           name: storeItem.name,
+      //         },
+      //         unit_amount: storeItem.priceInCents,
+      //       },
+      //       quantity: item.quantity,
+      //     };
+      //   }),
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
